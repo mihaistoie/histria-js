825989e277c7f4d680294aebfd527d52ae732174
misto <mihaistoie@gmail.com>
2017-01-08 12:04:03 +0100

    #9 test model for one-to-one compositions

---
+++ test/compositions/model/car.ts
@@ -0,0 +1,81 @@
+import {
+	Instance, InstanceState, InstanceErrors, ModelManager,
+	ErrorState, State, StringState, IntegerState, EnumState, NumberState, DateState, DateTimeState, RefArrayState, RefObjectState,
+	IntegerValue, NumberValue
+} from '../../../src/index';
+import { Engine } from './engine';
+
+
+export class Car extends Instance {
+	protected init() {
+		super.init();
+		let that = this;
+		that._schema = CAR_SCHEMA;
+	}
+	protected createStates() {
+		let that = this;
+		that._states = new CarState(that, that._schema);
+	}
+	protected createErrors() {
+		let that = this;
+		that._errors = new CarErrors(that, that._schema);
+	}
+	public get id(): IntegerValue {
+		return this._children.id;
+	}
+	public engine(value?: Engine): Promise<Engine> {
+		return this._children.engine.value(value);
+	}
+	public get $states(): CarState {
+		return <CarState>this._states;
+	}
+	public get $errors(): CarErrors {
+		return <CarErrors>this._errors;
+	}
+}
+
+export class CarErrors extends InstanceErrors {
+	public get $(): ErrorState {
+		return this._messages.$;
+	}
+	public get id(): ErrorState {
+		return this._messages.id;
+	}
+	public get engine(): ErrorState {
+		return this._messages.engine;
+	}
+}
+
+export class CarState extends InstanceState {
+	public get id(): IntegerState {
+		return this._states.id;
+	}
+}
+const
+	CAR_SCHEMA = {
+		"type": "object",
+		"name": "car",
+		"properties": {
+			"id": {
+				"type": "integer",
+				"generated": true
+			}
+		},
+		"relations": {
+			"engine": {
+				"type": "hasOne",
+				"model": "engine",
+				"aggregationKind": "composite",
+				"invRel": "car",
+				"title": "engine",
+				"localFields": [
+					"id"
+				],
+				"foreignFields": [
+					"carId"
+				]
+			}
+		},
+		"nameSpace": "Car"
+	};
+new ModelManager().registerClass(Car, CAR_SCHEMA.name, CAR_SCHEMA.nameSpace);
---
+++ test/compositions/model/engine.ts
@@ -0,0 +1,94 @@
+import {
+	Instance, InstanceState, InstanceErrors, ModelManager,
+	ErrorState, State, StringState, IntegerState, EnumState, NumberState, DateState, DateTimeState, RefArrayState, RefObjectState,
+	IntegerValue, NumberValue
+} from '../../../src/index';
+import { Car } from './car';
+
+
+export class Engine extends Instance {
+	protected init() {
+		super.init();
+		let that = this;
+		that._schema = ENGINE_SCHEMA;
+	}
+	protected createStates() {
+		let that = this;
+		that._states = new EngineState(that, that._schema);
+	}
+	protected createErrors() {
+		let that = this;
+		that._errors = new EngineErrors(that, that._schema);
+	}
+	public get id(): IntegerValue {
+		return this._children.id;
+	}
+	public get carId(): IntegerValue {
+		return this._children.carId;
+	}
+	public car(): Promise<Car> {
+		return this._children.car.value();
+	}
+	public get $states(): EngineState {
+		return <EngineState>this._states;
+	}
+	public get $errors(): EngineErrors {
+		return <EngineErrors>this._errors;
+	}
+}
+
+export class EngineErrors extends InstanceErrors {
+	public get $(): ErrorState {
+		return this._messages.$;
+	}
+	public get id(): ErrorState {
+		return this._messages.id;
+	}
+	public get carId(): ErrorState {
+		return this._messages.carId;
+	}
+	public get car(): ErrorState {
+		return this._messages.car;
+	}
+}
+
+export class EngineState extends InstanceState {
+	public get id(): IntegerState {
+		return this._states.id;
+	}
+	public get carId(): IntegerState {
+		return this._states.carId;
+	}
+}
+const
+	ENGINE_SCHEMA = {
+		"type": "object",
+		"name": "engine",
+		"properties": {
+			"id": {
+				"type": "integer",
+				"generated": true
+			},
+			"carId": {
+				"type": "integer",
+				"isReadOnly": true
+			}
+		},
+		"relations": {
+			"car": {
+				"type": "belongsTo",
+				"model": "car",
+				"aggregationKind": "composite",
+				"invRel": "engine",
+				"title": "car",
+				"localFields": [
+					"carId"
+				],
+				"foreignFields": [
+					"id"
+				]
+			}
+		},
+		"nameSpace": "Engine"
+	};
+new ModelManager().registerClass(Engine, ENGINE_SCHEMA.name, ENGINE_SCHEMA.nameSpace);

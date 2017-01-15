a18f70966913f3819cc186420e06d09a6377d84b
misto <mihaistoie@gmail.com>
2017-01-15 15:28:22 +0100

    unused files removes

--- definitions/lib/consts/consts.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const RULE_TRIGGERS: {
-    PROP_CHANGED: string;
-    INIT: string;
-};
--- definitions/lib/model/user-context.d.ts
+++
@@ -1,5 +0,0 @@
-import { UserContext } from './instance';
-export declare class TranContext implements UserContext {
-    private _lang;
-    private _country;
-}

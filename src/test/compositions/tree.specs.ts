cda87239d6355181e5c30c31f1450d71956a5dad
Mihai  STOIE <mihaistoie@gmail.com>
2017-04-13 09:31:49 +0200

    #31 done

--- src/test/compositions/tree.specs.ts
+++ src/test/compositions/tree.specs.ts
@@ -35,7 +35,6 @@ async function testCreate(): Promise<void> {
 
 
 async function testLoad(): Promise<void> {
-    /*
     let transaction = new Transaction();
 
     let child01 = await transaction.load<Tree>(Tree, { id: 102, parentId: 100 });
@@ -47,37 +46,26 @@ async function testLoad(): Promise<void> {
 
     assert.equal(children.length, 2, 'Root has 2 children');
     let data1 = transaction.saveToJson();
-    console.log(data1.instances[0].data);
     transaction.clear();
     await transaction.loadFromJson(data1, false);
     let data2 = transaction.saveToJson();
     assert.deepEqual(data1, data2, 'Restore test in restore');
-    */
+
+    let root = await transaction.findOne<Tree>(Tree, { id: 100 })
+    children = await root.leafs.toArray();
+
+    assert.equal(children.length, 2, 'Root has 2 children (2)');
+    transaction.clear();
 }
 
 
 async function testRestore(): Promise<void> {
-    /*
     let transaction = new Transaction();
-    let order1 = await transaction.load<Order>(Order, { id: 101, totalAmount: 0, items: [{ id: 1, amount: 0 }, { id: 2, amount: 0 }, { id: 3, amount: 0 }] });
-    let children1 = await order1.items.toArray();
-    assert.equal(children1[1].loaded, true, '(1) Loaded = true Init rule called');
-    await children1[1].setLoaded(false);
-    assert.equal(children1[1].loaded, false, '(1) Loaded = false');
 
-    await children1[2].amount.setValue(10);
-    assert.equal(order1.totalAmount.value, 10, '(1) Rule called');
+    let child01 = await transaction.load<Tree>(Tree, { id: 102, parentId: 100 });
+    let child02 = await transaction.load<Tree>(Tree, { id: 101, parentId: 100 });
+    let root01 = await transaction.load<Tree>(Tree, { id: 100 });
 
-    let saved = JSON.parse(JSON.stringify(order1.model()));
-
-    let transaction2 = new Transaction();
-    let order2 = await transaction.restore<Order>(Order, saved, false);
-    let children2 = await order2.items.toArray();
-
-    assert.equal(children2.length, 3, 'Order has 3 items');
-    assert.equal(children2[1].loaded, false, '(2) Loaded = false');
-    await children2[0].amount.setValue(10);
-    assert.equal(order2.totalAmount.value, 20, '(2) Rule called');
 
     let data1 = transaction.saveToJson();
     transaction.clear();
@@ -85,40 +73,12 @@ async function testRestore(): Promise<void> {
     let data2 = transaction.saveToJson();
     assert.deepEqual(data1, data2, 'Restore test in restore');
 
-    transaction.destroy();
-    */
+    let root = await transaction.findOne<Tree>(Tree, { id: 100 })
+    let children = await root.leafs.toArray();
 
-}
-
-async function testRules(): Promise<void> {
-    /*
-    let transaction = new Transaction();
-    let order = await transaction.create<Order>(Order);
-    let item1 = await transaction.create<OrderItem>(OrderItem);
-    let item2 = await transaction.create<OrderItem>(OrderItem);
-    await order.items.add(item1);
-    await order.items.add(item2);
-    await item1.amount.setValue(10);
-    assert.equal(order.totalAmount.value, 10, 'Total amount  = 10');
-    await item2.amount.setValue(10);
-    assert.equal(order.totalAmount.value, 20, 'Total amount  = 20');
-    await item1.amount.setValue(5);
-    assert.equal(order.totalAmount.value, 15, 'Total amount  = 15');
-    await order.items.remove(item2);
-    assert.equal(order.totalAmount.value, 5, 'Total amount  = 5');
-    await item1.setOrder(null);
-    assert.equal(order.totalAmount.value, 0, 'Total amount  = 0');
-    await order.items.set([item1, item2]);
-    assert.equal(order.totalAmount.value, 15, 'Total amount  = 15');
-
-    let data1 = transaction.saveToJson();
+    assert.equal(children.length, 2, 'Root has 2 children (2)');
     transaction.clear();
-    await transaction.loadFromJson(data1, false);
-    let data2 = transaction.saveToJson();
-    assert.deepEqual(data1, data2, 'Restore test in rules');
 
-    transaction.destroy();
-    */
 }
 
 
@@ -141,16 +101,6 @@ describe('Tree, Composition', () => {
 
 
     });
-    it('Tree - rules', function (done) {
-        testRules().then(function () {
-            done();
-        }).catch(function (ex) {
-            done(ex);
-        })
-
-
-    });
-
     it('Tree - restore', function (done) {
         testRestore().then(function () {
             done();
--- test/compositions/tree.specs.js
+++ test/compositions/tree.specs.js
@@ -24,86 +24,36 @@ async function testCreate() {
     transaction.clear();
 }
 async function testLoad() {
-    /*
-    let transaction = new Transaction();
-
-    let child01 = await transaction.load<Tree>(Tree, { id: 102, parentId: 100 });
-    let child02 = await transaction.load<Tree>(Tree, { id: 101, parentId: 100 });
-    let root01 = await transaction.load<Tree>(Tree, { id: 100 });
-
-
+    let transaction = new index_1.Transaction();
+    let child01 = await transaction.load(compositions_model_1.Tree, { id: 102, parentId: 100 });
+    let child02 = await transaction.load(compositions_model_1.Tree, { id: 101, parentId: 100 });
+    let root01 = await transaction.load(compositions_model_1.Tree, { id: 100 });
     let children = await root01.leafs.toArray();
-
     assert.equal(children.length, 2, 'Root has 2 children');
     let data1 = transaction.saveToJson();
-    console.log(data1.instances[0].data);
     transaction.clear();
     await transaction.loadFromJson(data1, false);
     let data2 = transaction.saveToJson();
     assert.deepEqual(data1, data2, 'Restore test in restore');
-    */
+    let root = await transaction.findOne(compositions_model_1.Tree, { id: 100 });
+    children = await root.leafs.toArray();
+    assert.equal(children.length, 2, 'Root has 2 children (2)');
+    transaction.clear();
 }
 async function testRestore() {
-    /*
-    let transaction = new Transaction();
-    let order1 = await transaction.load<Order>(Order, { id: 101, totalAmount: 0, items: [{ id: 1, amount: 0 }, { id: 2, amount: 0 }, { id: 3, amount: 0 }] });
-    let children1 = await order1.items.toArray();
-    assert.equal(children1[1].loaded, true, '(1) Loaded = true Init rule called');
-    await children1[1].setLoaded(false);
-    assert.equal(children1[1].loaded, false, '(1) Loaded = false');
-
-    await children1[2].amount.setValue(10);
-    assert.equal(order1.totalAmount.value, 10, '(1) Rule called');
-
-    let saved = JSON.parse(JSON.stringify(order1.model()));
-
-    let transaction2 = new Transaction();
-    let order2 = await transaction.restore<Order>(Order, saved, false);
-    let children2 = await order2.items.toArray();
-
-    assert.equal(children2.length, 3, 'Order has 3 items');
-    assert.equal(children2[1].loaded, false, '(2) Loaded = false');
-    await children2[0].amount.setValue(10);
-    assert.equal(order2.totalAmount.value, 20, '(2) Rule called');
-
+    let transaction = new index_1.Transaction();
+    let child01 = await transaction.load(compositions_model_1.Tree, { id: 102, parentId: 100 });
+    let child02 = await transaction.load(compositions_model_1.Tree, { id: 101, parentId: 100 });
+    let root01 = await transaction.load(compositions_model_1.Tree, { id: 100 });
     let data1 = transaction.saveToJson();
     transaction.clear();
     await transaction.loadFromJson(data1, false);
     let data2 = transaction.saveToJson();
     assert.deepEqual(data1, data2, 'Restore test in restore');
-
-    transaction.destroy();
-    */
-}
-async function testRules() {
-    /*
-    let transaction = new Transaction();
-    let order = await transaction.create<Order>(Order);
-    let item1 = await transaction.create<OrderItem>(OrderItem);
-    let item2 = await transaction.create<OrderItem>(OrderItem);
-    await order.items.add(item1);
-    await order.items.add(item2);
-    await item1.amount.setValue(10);
-    assert.equal(order.totalAmount.value, 10, 'Total amount  = 10');
-    await item2.amount.setValue(10);
-    assert.equal(order.totalAmount.value, 20, 'Total amount  = 20');
-    await item1.amount.setValue(5);
-    assert.equal(order.totalAmount.value, 15, 'Total amount  = 15');
-    await order.items.remove(item2);
-    assert.equal(order.totalAmount.value, 5, 'Total amount  = 5');
-    await item1.setOrder(null);
-    assert.equal(order.totalAmount.value, 0, 'Total amount  = 0');
-    await order.items.set([item1, item2]);
-    assert.equal(order.totalAmount.value, 15, 'Total amount  = 15');
-
-    let data1 = transaction.saveToJson();
+    let root = await transaction.findOne(compositions_model_1.Tree, { id: 100 });
+    let children = await root.leafs.toArray();
+    assert.equal(children.length, 2, 'Root has 2 children (2)');
     transaction.clear();
-    await transaction.loadFromJson(data1, false);
-    let data2 = transaction.saveToJson();
-    assert.deepEqual(data1, data2, 'Restore test in rules');
-
-    transaction.destroy();
-    */
 }
 describe('Tree, Composition', () => {
     it('Tree - create', function (done) {
@@ -115,13 +65,6 @@ describe('Tree, Composition', () => {
     });
     it('Tree - load', function (done) {
         testLoad().then(function () {
-            done();
-        }).catch(function (ex) {
-            done(ex);
-        });
-    });
-    it('Tree - rules', function (done) {
-        testRules().then(function () {
             done();
         }).catch(function (ex) {
             done(ex);

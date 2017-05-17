2590cb5ccad69c5733f86e83eec5749127d9e305
Mihai  STOIE <mihaistoie@gmail.com>
2017-05-17 16:06:40 +0200

    #37 Views with HasOne<View> done

---
+++ src/test/view-one-view/address-view.ts
@@ -0,0 +1,106 @@
+import {
+    Instance, View, InstanceState, InstanceErrors, modelManager,
+    HasManyComposition, HasManyAggregation, HasManyRefObject,
+    ErrorState, State, StringState, IdState, BooleanState, IntegerState,
+    EnumState, NumberState, DateState, DateTimeState, RefArrayState, RefObjectState,
+    NumberValue
+} from '../../index';
+import { UserDetail } from './user-detail';
+
+
+export class AddressView extends View {
+    public static isPersistent: boolean = false;
+    public get street(): string {
+        return this.getPropertyByName('street');
+    }
+    public setStreet(value: string): Promise<string> {
+        return this.setPropertyByName('street', value);
+    }
+    public get id(): any {
+        return this._children.id.value;
+    }
+    public user(): Promise<UserDetail> {
+        return this._children.user.getValue();
+    }
+    public setUser(value: UserDetail): Promise<UserDetail> {
+        return this._children.user.setValue(value);
+    }
+    public get $states(): AddressViewState {
+        return <AddressViewState>this._states;
+    }
+    public get $errors(): AddressViewErrors {
+        return <AddressViewErrors>this._errors;
+    }
+    protected init() {
+        super.init();
+        let that = this;
+        that._schema = ADDRESSVIEW_SCHEMA;
+    }
+    protected createStates() {
+        let that = this;
+        that._states = new AddressViewState(that, that._schema);
+    }
+    protected createErrors() {
+        let that = this;
+        that._errors = new AddressViewErrors(that, that._schema);
+    }
+}
+
+export class AddressViewErrors extends InstanceErrors {
+    public get $(): ErrorState {
+        return this._messages.$;
+    }
+    public get street(): ErrorState {
+        return this._messages.street;
+    }
+    public get id(): ErrorState {
+        return this._messages.id;
+    }
+}
+
+export class AddressViewState extends InstanceState {
+    public get street(): StringState {
+        return this._states.street;
+    }
+    public get id(): IdState {
+        return this._states.id;
+    }
+}
+export const
+    ADDRESSVIEW_SCHEMA = {
+        name: 'AddressView',
+        type: 'object',
+        view: true,
+        nameSpace: 'view-has-one-view',
+        properties: {
+            street: {
+                title: 'Street',
+                type: 'string'
+            },
+            id: {
+                type: 'integer',
+                generated: true,
+                format: 'id'
+            }
+        },
+        relations: {
+            user: {
+                type: 'belongsTo',
+                model: 'UserDetail',
+                invRel: 'address',
+                nameSpace: 'view-has-one-view',
+                title: 'user',
+                aggregationKind: 'composite',
+                localFields: [
+                    'id'
+                ],
+                foreignFields: [
+                    'addressId'
+                ]
+            }
+        },
+        meta: {
+            parent: 'UserDetail',
+            parentRelation: 'user'
+        }
+    };
---
+++ test/view-one-view/address-view.js
@@ -0,0 +1,99 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("../../index");
+class AddressView extends index_1.View {
+    get street() {
+        return this.getPropertyByName('street');
+    }
+    setStreet(value) {
+        return this.setPropertyByName('street', value);
+    }
+    get id() {
+        return this._children.id.value;
+    }
+    user() {
+        return this._children.user.getValue();
+    }
+    setUser(value) {
+        return this._children.user.setValue(value);
+    }
+    get $states() {
+        return this._states;
+    }
+    get $errors() {
+        return this._errors;
+    }
+    init() {
+        super.init();
+        let that = this;
+        that._schema = exports.ADDRESSVIEW_SCHEMA;
+    }
+    createStates() {
+        let that = this;
+        that._states = new AddressViewState(that, that._schema);
+    }
+    createErrors() {
+        let that = this;
+        that._errors = new AddressViewErrors(that, that._schema);
+    }
+}
+AddressView.isPersistent = false;
+exports.AddressView = AddressView;
+class AddressViewErrors extends index_1.InstanceErrors {
+    get $() {
+        return this._messages.$;
+    }
+    get street() {
+        return this._messages.street;
+    }
+    get id() {
+        return this._messages.id;
+    }
+}
+exports.AddressViewErrors = AddressViewErrors;
+class AddressViewState extends index_1.InstanceState {
+    get street() {
+        return this._states.street;
+    }
+    get id() {
+        return this._states.id;
+    }
+}
+exports.AddressViewState = AddressViewState;
+exports.ADDRESSVIEW_SCHEMA = {
+    name: 'AddressView',
+    type: 'object',
+    view: true,
+    nameSpace: 'view-has-one-view',
+    properties: {
+        street: {
+            title: 'Street',
+            type: 'string'
+        },
+        id: {
+            type: 'integer',
+            generated: true,
+            format: 'id'
+        }
+    },
+    relations: {
+        user: {
+            type: 'belongsTo',
+            model: 'UserDetail',
+            invRel: 'address',
+            nameSpace: 'view-has-one-view',
+            title: 'user',
+            aggregationKind: 'composite',
+            localFields: [
+                'id'
+            ],
+            foreignFields: [
+                'addressId'
+            ]
+        }
+    },
+    meta: {
+        parent: 'UserDetail',
+        parentRelation: 'user'
+    }
+};
